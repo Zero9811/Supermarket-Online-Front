47b09354741885a0ba2fe109fc7ce6a66272b26b
sean <itseanzhong@gmail.com>
2019-04-28 00:40:09 +0800

    项目资源初始化成功

---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>home</div>
+    <router-link to="./login">登录页面</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
