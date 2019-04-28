1fc41d1854a1f93934f013d10fb37c064645ed19
sean <itseanzhong@gmail.com>
2019-04-28 11:03:53 +0800

    Header组件页面样式开发完成

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
+    <home-header></home-header>
     <div>home</div>
     <router-link to="./login">登录页面</router-link>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
